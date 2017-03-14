f3a2db3aa0449714e134b1cc457b3877c9a3f1bf
MasterTos <MasterTos@yahoo.com>
2017-03-14 20:59:25 +0700

    calloc and malloc

--- memory-allocation.c
+++ memory-allocation.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+#define PAGE_SIZE (uint64_t)(1 << 12)
+
+typedef struct {
+    uint8_t * page;
+    uint64_t size;
+} Page;
+
 int main() {
-    
+    int i;
+    Page * page_ptr = calloc(10, PAGE_SIZE);
+    for( i = 0 ; i < 10 ; i++) {
+        page_ptr[i].page = (uint8_t *)malloc(sizeof(uint8_t)*PAGE_SIZE);
+        page_ptr[i].size = sizeof(uint8_t) * PAGE_SIZE;
+    }
+
 }
