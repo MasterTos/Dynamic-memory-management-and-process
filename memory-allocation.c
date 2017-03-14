d456fa8ae1be39a4fb4fc1428dad55a078cadff2
MasterTos <MasterTos@yahoo.com>
2017-03-14 22:42:23 +0700

    realloc in process

--- memory-allocation.c
+++ memory-allocation.c
@@ -8,17 +8,19 @@
 
 #define MULTIPLE_SIZE 10
 
-uint64_t page_ptr_size(uint64_t size);
-
 typedef struct
 {
     uint8_t * page;
     uint64_t size;
 } Page;
 
+uint64_t page_ptr_size(uint64_t size);
+int realloc_page_ptr(Page * page_ptr, uint64_t current_size, uint64_t new_size);
+void free_all(Page * page_ptr, uint64_t size);
+
 int main()
 {
-    int i, input = 1, current_size = MULTIPLE_SIZE;
+    int i, input, current_size = MULTIPLE_SIZE;
 
     Page * page_ptr = calloc(page_ptr_size(current_size), PAGE_SIZE);
 
@@ -26,9 +28,10 @@ int main()
     {
         page_ptr[i].page = (uint8_t *)malloc(sizeof(uint8_t)*PAGE_SIZE);
         page_ptr[i].size = sizeof(uint8_t) * PAGE_SIZE;
+        printf("%d %" PRIx64 "\n", i, page_ptr[i]);
     }
 
-    while(input > 0)
+    while(1)
     {
         int a;
         printf("Please enter new page_ptr size (enter 0 to exit) : ");
@@ -37,9 +40,41 @@ int main()
             fprintf(stderr, "Error, Invalid input!!\n");
             exit(EXIT_FAILURE);
         }
-        current_size = realloc_page_ptr(page_ptr, current_size, input);
+        if(input <= 0) break;
+        //current_size = realloc_page_ptr(page_ptr, current_size, input);
+
+        int new_size = input;
+        if(new_size < current_size) {
+        int i;
+        for(i = new_size ; i < current_size ; i++) {
+                printf("%" PRIx64 " %d\n", page_ptr[i], page_ptr[i].size);
+            free(page_ptr[i].page);
+            page_ptr[i].size = 0;
+        }
+    }
+    printf("page_ptr address before realloc :%" PRIx64 "\n", page_ptr[0]);
+    if((page_ptr = realloc(page_ptr, page_ptr_size(new_size)*PAGE_SIZE)) == NULL)
+    {
+        fprintf(stderr, "Error, (re)allocation memory!!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(current_size < new_size) {
+        int i;
+        for( i = current_size ; i < new_size ; i++) {
+            page_ptr[i].page = (uint8_t *)malloc(sizeof(uint8_t)*PAGE_SIZE);
+            page_ptr[i].size = sizeof(uint8_t) * PAGE_SIZE;
+        }
+    }
+    printf("page_ptr address after realloc :%" PRIx64 "\n", page_ptr[0]);
+
+        current_size = new_size;
+
+
 
     }
+    free_all(page_ptr, current_size);
+
 
     return 0;
 }
@@ -49,17 +84,20 @@ uint64_t page_ptr_size(uint64_t size)
     return (uint64_t)(ceil((float)size / MULTIPLE_SIZE)* MULTIPLE_SIZE);
 }
 
-int realloc_page_ptr(Page * page_ptr, uint64_t current_size, uint64_t new_size)
+int realloc_page_ptr(Page *page_ptr, uint64_t current_size, uint64_t new_size)
 {
+    printf("page_ptr address before realloc :%" PRIx64 "\n", page_ptr[0]);
     if(current_size == new_size) return new_size;
     else if(new_size < current_size) {
         int i;
-        for(i = new_size - 1 ; i < current_size ; i++) {
+        for(i = new_size ; i < current_size ; i++) {
+                printf("%" PRIx64 " %d\n", page_ptr[i], page_ptr[i].size);
             free(page_ptr[i].page);
             page_ptr[i].size = 0;
         }
     }
-    else if(realloc(page_ptr, page_ptr_size(new_size)) != NULL)
+
+    if((page_ptr = realloc(page_ptr, page_ptr_size(new_size)*PAGE_SIZE)) == NULL)
     {
         fprintf(stderr, "Error, (re)allocation memory!!\n");
         exit(EXIT_FAILURE);
@@ -67,11 +105,23 @@ int realloc_page_ptr(Page * page_ptr, uint64_t current_size, uint64_t new_size)
 
     if(current_size < new_size) {
         int i;
-        for( i = current_size - 1 ; i < new_size ; i++) {
+        for( i = current_size ; i < new_size ; i++) {
             page_ptr[i].page = (uint8_t *)malloc(sizeof(uint8_t)*PAGE_SIZE);
             page_ptr[i].size = sizeof(uint8_t) * PAGE_SIZE;
         }
     }
+    printf("page_ptr address after realloc :%" PRIx64 "\n", page_ptr[0]);
 
         return new_size;
+}
+
+void free_all(Page * page_ptr, uint64_t size) {
+    //free(page_ptr);
+    int i;
+    for(i = 0 ; i < size ; i++) {
+        free(page_ptr[i].page);
+        //page_ptr[i].size = 0;
+        printf("%d %" PRIx64 "\n", i, page_ptr[i]);
+    }
+    free(page_ptr);
 }
