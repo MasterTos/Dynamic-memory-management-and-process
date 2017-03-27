9cc6ec465f9f264d2e56b70747b649f82395b0bc
MasterTos <MasterTos@yahoo.com>
2017-03-27 23:50:45 +0700

    try goto statement

--- fork-process.c
+++ fork-process.c
@@ -2,7 +2,45 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+//#include "apue.h"
 
-int main() {
-    
+#define PRINT printf("PID: %d  PPID: %d\n", (int)getpid(), (int)getppid());
+
+void pr_exit(int status);
+
+static int a;
+
+int main(int argc, char* argv[]) {
+    int N = atoi(argv[1]);
+    pid_t child_pid;
+    int count = 0;
+LOOP_FORK :
+    if(child_pid != 0) {
+        if(count == 5) return 0;
+        count++;
+        printf("parent L ");
+        PRINT;
+        wait(NULL);
+    } else {
+        printf("child : ");
+        PRINT;
+        child_pid = fork();
+        goto LOOP_FORK;
+    }
+
+    return 0;
+}
+
+void pr_exit(int status) {
+    if (WIFEXITED(status))
+        printf("normal termination, exit status = %d\n", WEXITSTATUS(status));
+    else if (WIFSIGNALED(status))
+        printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status),
+#ifdef WCOREDUMP
+               WCOREDUMP(status) ? " (core file generated)" : "");
+#else
+               "");
+#endif
+    else if (WIFSTOPPED(status))
+        printf("child stopped, signal number = %d\n", WSTOPSIG(status));
 }
